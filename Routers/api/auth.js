7927f30af6bd1aaed67002553bc95dc0c807cc23
guardian0215 <88623862+guardian0215@users.noreply.github.com>
2021-11-08 01:46:09 -0500

    log to catch error

--- Routers/api/auth.js
+++ Routers/api/auth.js
@@ -3,21 +3,19 @@ const passport = require('passport')
 const cfg = require('../../config')
 
 app.get('/discord', passport.authenticate('discord'));
-app.get('/discord/redirect', (req, res) => {
-    console.log("redirect req");
+app.get('/discord/redirect', (req, res, next) => {
+    try {
+        passport.authenticate('discord', {
+            failureRedirect: `${cfg.FRONDEND_URL}/`
+        })
+        next();
+    }
+    catch (error) {
+        console.log("error", error)
+    }
+}, (req,res) => {
+    console.log("req", req);
     res.redirect(`${cfg.FRONDEND_URL}/account`)
-    // try {
-    //     console.log("redirect req", req);
-    //     passport.authenticate('discord', {
-    //         failureRedirect: `${cfg.FRONDEND_URL}/`
-    //     }), (req,res) => {
-    //         console.log("req", req);
-    //         res.redirect(`${cfg.FRONDEND_URL}/account`)
-    //     }
-    // }
-    // catch (error) {
-    //     console.log("error", error)
-    // }
 })
 
 app.get('/', (req, res) => {
