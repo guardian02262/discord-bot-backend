b8682c461b48acec6b0410067d3ca86c313b088f
guardian0215 <88623862+guardian0215@users.noreply.github.com>
2021-11-07 22:17:58 -0500

    modify protoool

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    token: "OTA1NzA4NTY4OTE1ODgyMDA0.YYOA1g.hVajgP_LO6Y4TxZWr0yLgzsGNAk", // token from bot
+    token: "OTA2NzU4OTE4NzU0MTQ4MzYz.YYdTDA.Z3sJhkemGJ0753UFxIRpcXgA_B8", // token from bot
     prefix: "v-", // default prefix
     database: "mongodb+srv://guardian:ASDFasdf1234@cluster0.i86ba.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", // link from mongodb
-    clientId: "905708568915882004", //client id from bot
-    clientSecret: "t6hU8wBcBzPLwn_XD430SrcbpimZc-bz", // client secret from bot
-    callbackurl: "https://guardian-discord-bot-backend.herokuapp.com/api/auth/discord/redirect", // callback
+    clientId: "906758918754148363", //client id from bot
+    clientSecret: "8V7ZVnC3ImpLWiE6B7O-ZvsowWytuKdn", // client secret from bot
+    callbackurl: "http://guardian-discord-bot-backend.herokuapp.com/api/auth/discord/redirect", // callback
     port: 8000, // port
-    FRONDEND_URL: "https://guardian-discord-bot-frontend.herokuapp.com" // Front-End URL
+    FRONDEND_URL: "http://guardian-discord-bot-frontend.herokuapp.com" // Front-End URL
 }
