6f6541158c2ceda91cf8845efa1e097945c1106f
guardian0215 <88623862+guardian0215@users.noreply.github.com>
2021-11-08 18:07:35 -0500

    change redirect

--- Routers/api/auth.js
+++ Routers/api/auth.js
@@ -6,7 +6,7 @@ app.get('/discord', passport.authenticate('discord'));
 app.get('/discord/redirect', passport.authenticate('discord', {
     failureRedirect: `${cfg.FRONDEND_URL}/`
 }), (req,res) => {
-    res.redirect(`${cfg.FRONDEND_URL}/account`)
+    res.redirect(`${cfg.FRONDEND_URL}/server/1`)
 })
 
 app.get('/', (req, res) => {
