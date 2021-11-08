a19561fe32472417e05b25a900ceccbb5be9d120
guardian0215 <88623862+guardian0215@users.noreply.github.com>
2021-11-08 04:46:26 -0500

    log for auth redirect

--- Routers/api/auth.js
+++ Routers/api/auth.js
@@ -6,7 +6,7 @@ app.get('/discord', passport.authenticate('discord'));
 app.get('/discord/redirect', passport.authenticate('discord', {
     failureRedirect: `${cfg.FRONDEND_URL}/`
 }), (req,res) => {
-    console.log("redirect")
+    console.log("redirect", req)
     res.redirect(`${cfg.FRONDEND_URL}/account`)
 })
 
