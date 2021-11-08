ce77eec68db1de004edc0207806a7d15f7b7a903
guardian0215 <88623862+guardian0215@users.noreply.github.com>
2021-11-08 17:49:35 -0500

    chnage url

--- Routers/api/auth.js
+++ Routers/api/auth.js
@@ -6,20 +6,17 @@ app.get('/discord', passport.authenticate('discord'));
 app.get('/discord/redirect', passport.authenticate('discord', {
     failureRedirect: `${cfg.FRONDEND_URL}/`
 }), (req,res) => {
-    res.redirect(`/api/auth`)
+    res.redirect(`${cfg.FRONDEND_URL}/account`)
 })
 
 app.get('/', (req, res) => {
     if (req.user) {
         console.log("authorized")
-        req.msg = "authorized"
-        // res.json({ msg: "authorized", user: req.user })
+        res.json({ msg: "authorized", user: req.user })
     } else {
         console.log("unauthorized")
-        req.msg = "unauthorized"
-        // res.json({ msg: "unauthorized" })
+        res.json({ msg: "unauthorized" })
     }
-    res.redirect(`${cfg.FRONDEND_URL}/account`)
 })
 
 module.exports = app;
