1273490785ce360da1def521263557635547b067
guardian0215 <88623862+guardian0215@users.noreply.github.com>
2021-11-07 23:30:11 -0500

    modify cors method

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(passport.session());
 //     origin: ["http://guardian-discord-bot-frontend.herokuapp.com"],
 //     credentials: true
 // }))
-app.use(cors())
+app.use(cors({ origin: '*' }))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.get('/test', (req, res) => res.json({ msg: "success" }))
